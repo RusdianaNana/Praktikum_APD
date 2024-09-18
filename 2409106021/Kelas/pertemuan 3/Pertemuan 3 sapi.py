3d609ffe0c45b80224e934c462ce4dc8e8646f4f
RusdianaNana <rusdiana30122005@gmail.com>
2024-09-18 05:21:19 -0700

    Add files via upload

---
+++ 2409106021/Kelas/pertemuan 3/Pertemuan 3 sapi.py
@@ -0,0 +1,11 @@
+a = int(input("masukkan angka 1 : "))
+b= int(input("masukkan angka kedua 2 : "))
+
+if a > b :
+    print("kelinci")
+if a < b :
+        print("kura-kura")
+if a == b :
+            print("kerbau")
+else:
+    print("invalid")
